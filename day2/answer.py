b13f13eca3688d5798c959c640bc35210d0f4ecc
podpah <stream_bars07@icloud.com>
2023-12-02 16:53:06 +0000

    Merging day 2 part 1 + 2

--- day2/answer.py
+++ day2/answer.py
@@ -2,6 +2,7 @@ f = open("day2/input.txt")
 data = f.read()
 array = data.split('\n')
 
+game_sum = 0
 sum = 0
 for each in array:
     game_id = int(each.split(":")[0].split(" ")[1])
@@ -12,5 +13,8 @@ for each in array:
         num = int(num)
         rgb[colour] = max(rgb[colour],num)
     sum += rgb["green"] * rgb["red"]*rgb["blue"]
+    if rgb["red"] <= 12 and rgb["green"] <= 13 and rgb["blue"] <= 14:
+        game_sum += game_id
 
 print(sum)
+print(game_sum)
--- day2/notes.md
+++ day2/notes.md
@@ -1,2 +1,4 @@
 I took so long on part 1 as I misread the question
 Part 2 was quite quick
+
+I ended up merging them just because
