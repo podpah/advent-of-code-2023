cd748b1c4239052040cd936552e1e28250742538
podpah <stream_bars07@icloud.com>
2023-12-02 13:44:05 +0000

    Day 2 done

---
+++ day2/notes.md
@@ -0,0 +1,2 @@
+I took so long on part 1 as I misread the question
+Part 2 was quite quick
--- day2/part2.py
+++ day2/part2.py
@@ -1,2 +1,16 @@
-f = open("input.txt")
+f = open("day2/input.txt")
 data = f.read()
+array = data.split('\n')
+
+sum = 0
+for each in array:
+    game_id = int(each.split(":")[0].split(" ")[1])
+    values = each.split(":")[1].replace(";",",").split(",")
+    rgb = {"red":0, "green":0, "blue":0}
+    for value in values:
+        num, colour = value.strip().split(" ")
+        num = int(num)
+        rgb[colour] = max(rgb[colour],num)
+    sum += rgb["green"] * rgb["red"]*rgb["blue"]
+
+print(sum)
