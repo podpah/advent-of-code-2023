b9db5bb2797754feb8274192b517f506762104dd
podpah <pedrinhohrodrigues@outlook.com>
2023-12-04 17:43:11 +0000

    Day 4 part 1 added

---
+++ day3/notes.md
@@ -0,0 +1,1 @@
+No. Day 3 just looked like a major hassle so I skipped it
--- day4/part1.py
+++ day4/part1.py
@@ -1,2 +1,22 @@
-f = open("input.txt")
+f = open("day4/input.txt")
 data = f.read()
+array = data.split('\n')
+
+sum = 0
+for each in array:
+    card, values = each.split(":")
+    card_id = card[5:]
+
+    winning_nums, guesses = values.split("|")
+    winning_nums = [int(nums) for nums in winning_nums.strip().replace("  ", " ").split(" ")] # List Comprehension
+    guesses = [int(nums) for nums in guesses.strip().replace("  ", " ").split(" ") ] # List Comprehension
+
+    score = 0
+    for num in guesses:
+        if num in winning_nums:
+            score += 1
+    num = 1 if score else 0
+    for r in range(score-1):
+        num *= 2
+    sum += num
+print(sum)
