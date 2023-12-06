e656a3a2710cd9fb01dc45311ed5e10ee089f6fa
podpah <pedrinhohrodrigues@outlook.com>
2023-12-06 12:17:23 +0000

    Day 6 done

--- day6/part1.py
+++ day6/part1.py
@@ -1,2 +1,18 @@
-f = open("input.txt")
+f = open("day6/input.txt")
 data = f.read()
+
+times, distances = [line.split(":")[1].split() for line in data.split("\n")]
+both = zip(times, distances)
+
+total = 1
+for each in both:
+    sum = 0
+    duration = int(each[0])
+    distance = int(each[1])
+
+    for wait in range(duration + 1):
+        distance_travelled = (duration-wait)*wait
+        if distance_travelled > distance:
+            sum += 1
+    total *= sum
+print(total)
--- day6/part2.py
+++ day6/part2.py
@@ -1,2 +1,12 @@
-f = open("input.txt")
+f = open("day6/input.txt")
 data = f.read()
+
+time, distance = [int(line.split(":")[1].replace(" ","")) for line in data.split("\n")]
+
+sum = 0
+for wait in range(time + 1):
+    distance_travelled = (time-wait)*wait
+    if distance_travelled > distance:
+        sum += 1
+
+print(sum)
